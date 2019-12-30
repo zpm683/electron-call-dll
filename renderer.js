6db1879f55970cb21971d7601c7be778ba570ba2
下午茶溢栈 <zpm683@qq.com>
2019-12-30 16:52:39 +0900

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const demo = require('./build/Release/demo.node')
 
-document.write("6+6= "+ demo.add());
+const addon = require('bindings')('addon.node')
+document.write("1+2= "+ addon.add(1,2));
